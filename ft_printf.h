3b07767593907b47d420c9b77e054b4c911cc8ed
Kari_ma <121245611+KariHab@users.noreply.github.com>
2023-02-10 09:20:57 -0500

    Add files via upload

--- ft_printf.h
+++ ft_printf.h
@@ -13,7 +13,6 @@
 #	ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-// POUR AVOIR LES VA_TRUC
 # include <stdarg.h>
 # include <stdio.h>
 # include <unistd.h>
@@ -23,6 +22,5 @@ int		ft_printf(const char *s, ...);
 int		ft_printu(unsigned int nb);
 int		ft_printptr(uintptr_t ptr);
 int		ft_printhex(unsigned int nbr, char specifier);
-// char	*ft_convert_nbr(unsigned long long nbr, int base);
 
 #endif
